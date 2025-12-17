42fd44104875ac966e69d2eb8d1a4b0c6835cc74
jeffreyvarga <jwvarga@me.com>
2025-12-17 02:18:48 +0000

    Deploying to gh-pages from @ jeffreyvarga/jeffreyvarga.github.io@f1750c4dcf62010cad3fbf53c2678801d99cd87d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -4,20 +4,20 @@ const ninja = document.querySelector('ninja-keys');
 // add the home and posts menu items
 ninja.data = [{
     id: "nav-about",
-    title: "about",
+    title: "About",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-cv",
-          title: "cv",
+  },{id: "nav-curriculum-vitae",
+          title: "Curriculum Vitae",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
           },
         },{id: "nav-projects",
-          title: "projects",
+          title: "Projects",
           description: "A growing collection of your cool projects.",
           section: "Navigation",
           handler: () => {
