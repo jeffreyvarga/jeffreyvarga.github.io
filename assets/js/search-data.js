242d45734d090338c004a41b2c38dabf28c1509f
jeffreyvarga <jwvarga@me.com>
2026-01-01 04:09:47 +0000

    Deploying to gh-pages from @ jeffreyvarga/jeffreyvarga.github.io@b500ee3e5ead92e5ad0043246328a481911d6664 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -81,6 +81,11 @@ ninja.data = [{
           description: "Proposed Vertical Evacuation Options in Ocean Shores East, Wasington.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/OSEast/";
+            },},{id: "projects-ufo-sightings-in-california",
+          title: 'UFO Sightings in California',
+          description: "Quantitative Spatial Statistical Analysis of UFO Sightings in California",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/UFO/";
             },},{
         id: 'social-cv',
         title: 'CV',
